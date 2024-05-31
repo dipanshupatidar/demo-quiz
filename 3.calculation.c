38faec7c60fa6a98f3f991e0399af2540bea2233
dipanshupatidar <dipmukati14@gmail.com>
2024-05-31 23:48:13 +0530

    Add files via upload

---
+++ 3.calculation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+int radius;
+
+printf("enter a radious \n");
+scanf("%d", &radius);
+
+float area;
+area =3.14*radius*radius;
+
+printf("area is %f \n ", area);
+
+    return 0;
+}
